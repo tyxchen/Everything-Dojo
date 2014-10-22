8ded5288704e28ab8d9f1d4efa703dc3325b558c
tyxchen <tyxchen@outlook.com>
2014-10-21 22:54:46 -0400

    Use minimist, stop Gulp on linting error, ...
    ... update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -14,22 +14,27 @@ var gulp    = require("gulp"),
     jshint  = require("gulp-jshint"),
     uglify  = require("gulp-uglify"),
     csslint = require("gulp-csslint"),
-    minify  = require("gulp-minify-css");
+    minify  = require("gulp-minify-css"),
+
+    // Regular Node
+    argv    = require("minimist")(process.argv.slice(2), {boolean: true});
 
 // Lint
 
 // JS
 gulp.task("lint-js", function () {
-  return gulp.src(gulp.env.src || "src/js/*.js")
+  return gulp.src(argv.src || "src/js/*.js")
     .pipe(jshint())
-    .pipe(jshint.reporter("default"));
+    .pipe(jshint.reporter("default"))
+    .pipe(jshint.reporter("fail"));
 });
 
 // CSS
 gulp.task("lint-css", function () {
-  return gulp.src(gulp.env.src || "src/css/*.css")
+  return gulp.src(argv.src || "src/css/*.css")
     .pipe(csslint(".csslintrc"))
-    .pipe(csslint.reporter());
+    .pipe(csslint.reporter())
+    .pipe(csslint.failReporter());
 });
 
 // Build
@@ -38,7 +43,7 @@ gulp.task("lint-css", function () {
 gulp.task("scripts", ["lint-js"], function () {
   return gulp.src("src/js/*.js")
     .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.env.type === "production" ? changed(uglify()) : gutil.noop())
+    .pipe(argv.env === "production" ? changed(uglify()) : gutil.noop())
     .pipe(gulp.dest("js/"));
 });
 
@@ -46,7 +51,7 @@ gulp.task("scripts", ["lint-js"], function () {
 gulp.task("styles", ["lint-css"], function () {
   return gulp.src("src/css/*.css")
     .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.env.type === "production" ? changed(minify()) : gutil.noop())
+    .pipe(argv.env === "production" ? changed(minify()) : gutil.noop())
     .pipe(gulp.dest("css/"));
 });
 
