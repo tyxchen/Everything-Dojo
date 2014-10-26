836b764026dae8b8dbf9572c9e13cf09d6511a55
tyxchen <tyxchen@outlook.com>
2014-10-25 22:46:32 -0400

    Update Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ var gulp    = require("gulp"),
 // JS
 gulp.task("lint-js", function () {
   return gulp.src(argv.src || "src/js/*.js")
+    .pipe(!argv.src ? changed("js/") : gutil.noop())
     .pipe(jshint())
     .pipe(jshint.reporter("default"))
     .pipe(jshint.reporter("fail"));
@@ -32,6 +33,7 @@ gulp.task("lint-js", function () {
 // CSS
 gulp.task("lint-css", function () {
   return gulp.src(argv.src || "src/css/*.css")
+    .pipe(!argv.src ? changed("css/") : gutil.noop())
     .pipe(csslint(".csslintrc"))
     .pipe(csslint.reporter())
     .pipe(csslint.failReporter());
@@ -42,7 +44,7 @@ gulp.task("lint-css", function () {
 // Scripts
 gulp.task("scripts", ["lint-js"], function () {
   return gulp.src("src/js/*.js")
-    .pipe(argv.env === "production" ? changed(uglify()) : gutil.noop())
+    .pipe(argv.env === "production" ? uglify() : gutil.noop())
     .pipe(gulp.dest("js/"));
 });
 
@@ -50,7 +52,7 @@ gulp.task("scripts", ["lint-js"], function () {
 gulp.task("styles", ["lint-css"], function () {
   return gulp.src("src/css/*.css")
     .pipe(rename({suffix: ".min"}))
-    .pipe(argv.env === "production" ? changed(minify()) : gutil.noop())
+    .pipe(argv.env === "production" ? minify() : gutil.noop())
     .pipe(gulp.dest("css/"));
 });
 
@@ -61,8 +63,8 @@ gulp.task("default", function () {
 
 // Watch files
 gulp.task("watch", function () {
-  if (!argv.nowatch) {
-    gulp.watch("src/css/*.css", ["styles"]);
+  if (!argv.hasOwnProperty("watch")) {
+    gulp.watch("src/css/*.css", ["styles"])
     gulp.watch("src/js/*.js", ["scripts"]);
   }
 });
