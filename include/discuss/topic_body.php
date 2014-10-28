55b0a446c9a00b9db806cd0221f168691707ce9d
encadyma <encadyma@gmail.com>
2014-10-27 17:04:08 -0700

    Fixed the offset in ThemeDB forum

--- include/discuss/topic_body.php
+++ include/discuss/topic_body.php
@@ -88,8 +88,8 @@
   <?php if (!empty($posts)) { ?>
     <?php $thankedposts = [];
     // indice starts at 1 since get_posts deletes the first element (the root post)
-    for ($i = 1; $i <= sizeof($posts); $i++) {
-      $post = $posts[$i]; var_export($post); ?>
+    for ($i = (($typearg == 1) ? 0 : 1); $i <= (($typearg == 1) ? sizeof($posts)-1 : sizeof($posts)); $i++) {
+      $post = $posts[$i];?>
       <div class="topic-reply" id="<?php echo $i; ?>">
         <?php if (isset($_GET["unicorns"])) { ?><img class="avatar" src=<?php echo "\"http://unicornify.appspot.com/avatar/" . md5(get_all_user(intval($post["user_id"]))["user_email"]) . "?s=128\"" ?>><?php } ?>
         <div class="topic-text topic-reply-text" <?php if($post == end($posts)) echo 'id="last"'; ?>>
