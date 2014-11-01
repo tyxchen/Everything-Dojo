525f66f267f0a9f207f7fb20c3627d8ca81a8942
tyxchen <tyxchen@outlook.com>
2014-10-31 22:47:34 -0400

    Add inline notifications to menu, styling
    :D

--- include/header.php
+++ include/header.php
@@ -103,27 +103,71 @@
           </nav>
           <?php } else { ?>
           <nav>
-          <?php if(isset($_SESSION['user_id'])) { ?>
+            <?php if(isset($_SESSION['user_id'])) {
+              global $notification;
+              $notification_unread_count = $notification->count_unread($_SESSION['user_id']);
+              $notification_data = $notification->get_notifications($_SESSION['user_id'], 1000);
+            ?>
             <div class="user"><img src="<?php echo gravatar($_SESSION['user_id']); ?>"><span class="user-notification-status<?php if (isset($notification_unread_count) && $notification_unread_count > 0) echo ' new'; ?>"></span><span class="user-info"><?php echo $_SESSION['user_name'] . (isset($notification_unread_count) ? "&nbsp;(<span class='notification-count'>$notification_unread_count)</span>" : ""); ?></span>
-              <ul class="user-menu">
-                <li><a href="javascript:;" onclick="show_notifications()" class="notification-link">Notifications <?php if (isset($notification_unread_count)) { echo "(<span class='notification-count'>$notification_unread_count</span>)"; } ?></a></li>
-                <hr>
-                <li><a href="/myaccount.php" class="menu-myaccount">My Account</a></li>
-                <li><a href="/mysettings.php" class="menu-mysettings">My Settings</a></li>
-                <?php if ($_SESSION['user_level'] >= 5) echo '<li><a href="/admin.php" class="menu-admincp">Admin CP</a></li>'; ?>
-                <hr>
-                <li><a href="/logout.php" class="menu-logout">Logout</a></li>
-              </ul>
+              <div class="user-menu">
+                <ul class="user-menu-inner">
+                  <li><a href="javascript:;" onclick="$(this).parent().next().toggle();$(this).toggleClass('hover');" class="user-link menu-notification">Notifications <?php if (isset($notification_unread_count)) { echo "(<span class='notification-count'>$notification_unread_count</span>)"; } ?></a></li>
+                  <div class="menu-notifications" style="display:none">
+                    <?php if (count($notification_data) == 0) { ?>
+                    <a href="javascript:;" class="user-link menu-notification-link menu-notification-none">
+                      <div id="menu-notification-0" class="menu-notification-item menu-notification-none">
+                        <div class="menu-notification-text">No notifications</div>
+                      </div>
+                    </a>
+                    <?php } else {
+                      echo '<a href="javascript:;" onClick="mark_all_read(' . $_SESSION['user_id'] . ')" class="user-link menu-notification-link menu-notification-mark-all-read">Mark all read</a>';
+                    ?>
+                    <div class="menu-notification-body">
+                    <?php
+                      for ($i = 0; $i < count($notification_data); $i++) {
+                        if ($i >= 5) break;
+                        $notif = $notification_data[$i];
+                        $notif_data = $notification->get_notif_obj($notif['notification_type'], $notif['item_id']);
+                      ?>
+                      <a href="<?php echo $notif_data['url']; ?>" class="user-link menu-notification-link" onClick="mark_read(<?php echo $notif['id']; ?>)">
+                        <div id="menu-notification-<?php echo $notif['id']; ?>" class="menu-notification-item <?php if($notif['read'] == 0){ echo 'menu-notification-unread'; }else{ echo 'menu-notification-read'; } ?> ">
+                          <div class="menu-notification-text">
+                            <?php echo $notif_data['data']['subject']; ?>
+                          </div>
+                          <p class="time">
+                             <?php echo date('D M d, Y g:i a', $notif['timestamp']); ?>
+                          </p>
+                        </div>
+                      </a>
+                      <?php } ?>
+                      <div id="notification-footer">
+                        <a href="notifications.php" class="user-link menu-notification-link">See All<?php print (count($notification_data) - 5 > 0) ? ("&nbsp;(" . (count($notification_data) - 5) . ")") : ""; ?></a>
+                      </div>
+                    </div>
+                    <?php } ?>
+                  </div>
+                  <hr>
+                  <li><a href="/myaccount.php" class="user-link menu-myaccount">My Account</a></li>
+                  <li><a href="/mysettings.php" class="user-link menu-mysettings">My Settings</a></li>
+                  <?php if ($_SESSION['user_level'] >= 5) echo '<li><a href="/admin.php" class="user-link menu-admincp">Admin CP</a></li>'; ?>
+                  <hr>
+                  <li><a href="/logout.php" class="user-link menu-logout">Logout</a></li>
+                </ul>
+              </div>
             </div>
             <?php } else { ?>
-              <ul id="actions-menu">
-                <li><a href="/login.php" class="menu-login">Login</a></li>
-                <li><a href="/register.php" class="menu-register">Register</a></li>
-              </ul>
+            <ul id="actions-menu">
+              <li><a href="/login.php" class="menu-link menu-login">Login</a></li>
+              <li><a href="/register.php" class="menu-link menu-register">Register</a></li>
+            </ul>
             <?php } ?>
             <script>
               $(".user").click(function () {
+                $(this).toggleClass("user-active");
                 $(".user-menu").toggle();
+              });
+              $(".user-menu").click(function (e) {
+                e.stopPropagation();
               });
             </script>
           <?php } ?>
